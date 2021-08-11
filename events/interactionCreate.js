b7bc0f0a7f194bcc0abd04e33a559c3c0f7416ea
Victor Tran <victor.tran@tempestas.io>
2021-08-11 18:54:49 -0400

    create interactionCreate event

---
+++ events/interactionCreate.js
@@ -0,0 +1,20 @@
+export const event = {
+  name: "interactionCreate",
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+
+    if (!interaction.client.commands.has(interaction.commandName)) return;
+
+    try {
+      await interaction.client.commands
+        .get(interaction.commandName)
+        .execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
+};
