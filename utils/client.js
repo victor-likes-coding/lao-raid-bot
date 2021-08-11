e5d4ed8ff8a5a51e24400613d020c2e630292371
Victor Tran <victor.tran@tempestas.io>
2021-08-11 18:46:15 -0400

    create a getter method for commands

--- utils/client.js
+++ utils/client.js
@@ -5,6 +5,14 @@ class Lucy {
     this.client.commands = new Collection();
   }
 
+  get commands() {
+    /**
+     * Returns a simplified array of objects of the command list instead of the discordjs Collections data type.
+     */
+    return Array.from(this.client.commands.values()).map(({ data }) =>
+      data.toJSON()
+    );
+  }
 }
 
 
