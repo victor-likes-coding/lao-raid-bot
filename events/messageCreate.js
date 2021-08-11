2fdefe31ada36076aa2f25403ab03a96b6f5ddcc
Victor Tran <victor.tran@tempestas.io>
2021-08-11 19:39:29 -0400

    filter out messages without prefix and bot msgs

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,9 +4,9 @@ export const event = {
   name: "messageCreate",
   async execute(message) {
     const { prefix } = config;
-    if (!message.content.startsWith(prefix) || message.user.bot) {
+    if (!message.content.startsWith(prefix) || message.member.user.bot) {
       return;
     }
-    console.log(message);
+    console.log(message.content);
   },
 };
