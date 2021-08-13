45585428c7cb7fd30738d085c5ec7280a094a5cd
Victor Tran <victor.tran@tempestas.io>
2021-08-13 12:56:05 -0400

    update reply to use an embed

--- commands/commands.js
+++ commands/commands.js
@@ -1,23 +1,22 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { MessageEmbed } from "discord.js";
+import { Message } from "../src/model/Message/Message.js";
 
 export const command = {
   data: new SlashCommandBuilder()
     .setName("commands")
     .setDescription("Provides an up to date list of commands from Lucy"),
   async execute(interaction) {
-    const fancyMessageFormat = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("Lucy's Slash Commands")
-      .setTimestamp()
-      .setFooter("\u00A9 Lucy Bot");
-    console.log("User entered /commands");
+    const message = new Message().addTitle("Lucy's Slash Command");
     const commands = interaction.client.commands;
-    for (const [commandName, _] of commands) {
+    for (const [title, _] of commands) {
       const { description } = _.data.toJSON();
-      fancyMessageFormat.addField(commandName, description);
+      message.addField({
+        title,
+        description,
+        inline: false,
+      });
     }
-    console.log(fancyMessageFormat);
-    await interaction.reply({ embeds: [fancyMessageFormat] });
+    await interaction.reply({ embeds: [message.content] });
   },
 };
