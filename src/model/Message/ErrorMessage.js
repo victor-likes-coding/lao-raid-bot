b1e6fca9b094f3fde8b69033074049151d55c67e
Victor Tran <victor.tran@tempestas.io>
2021-08-14 01:00:22 -0400

    update constructor params

--- src/model/Message/ErrorMessage.js
+++ src/model/Message/ErrorMessage.js
@@ -1,6 +1,7 @@
 import { LucyEmbed } from "./LucyEmbed.js";
 
 export class ErrorMessage extends LucyEmbed {
+  constructor(options = {}, reason = "", category = "") {
     super(options);
     this.content.setColor("red");
   }
