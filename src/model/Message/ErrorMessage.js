66c40f7004a554b60d9c216913677f716854d6a8
Victor Tran <victor.tran@tempestas.io>
2021-08-14 01:03:13 -0400

    create way to generate error code

--- src/model/Message/ErrorMessage.js
+++ src/model/Message/ErrorMessage.js
@@ -1,4 +1,5 @@
 import { LucyEmbed } from "./LucyEmbed.js";
+import { errors } from "../../errors/codes.js";
 
 export class ErrorMessage extends LucyEmbed {
   constructor(options = {}, reason = "", category = "") {
@@ -6,8 +7,36 @@ export class ErrorMessage extends LucyEmbed {
     this.content.setColor("#ff0000");
     this.reason = reason;
     this.category = category;
+    if (this.reason && this.category) {
+      this.__setError();
+    }
+  }
+
   setErrorReason(category, reason) {
     this.reason = reason;
     this.category = category;
+    this.__setError();
+  }
+
+  __setError() {
+    const errorText = this.__setErrorString();
+    this.addDescription(errorText);
+  }
+
+  __setErrorString() {
+    const { text, code } = this.getErrorReason();
+    return `${this.category.toUpperCase()}_ERROR ${code}: ${text}`;
+  }
+
+  getErrorReason() {
+    console.log(this.category, this.reason);
+    if (this.category && this.reason) {
+      const error = errors[this.category][this.reason];
+      return error;
+    }
+    this.reason = "MISS_REQ_PARAMS";
+    this.category = "internal";
+    const error = this.__setErrorString();
+    throw Error(error);
   }
 }
