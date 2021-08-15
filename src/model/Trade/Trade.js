d0ef9fe21b73d310b31646a9fe67998df2d6720d
Victor Tran <victor.tran@tempestas.io>
2021-08-14 20:23:43 -0400

    move some util fns and add getter/setters

--- src/model/Trade/Trade.js
+++ src/model/Trade/Trade.js
@@ -1,24 +1,29 @@
 import moment from "moment";
 import { errors } from "../../errors/codes.js";
+import { round, toPercent } from "../../../utils/utils.js";
 
 import { v4 } from "uuid";
 
 export class Trade {
   constructor({
-    date = moment().day("friday").format("MM/DD"),
+    date = moment().day(12).format("MM/DD"),
     ticker,
     strike,
     type = "c",
-    price,
+    price = 0,
     amount = 1,
     owner,
+    average = 0,
+    profit = 0,
+    status = true,
+    cost = 0,
+    profitPercent = 0,
   }) {
     try {
-      this.__ticker = `$${ticker.toUpperCase()}`;
+      this.__ticker = `${ticker.toUpperCase()}`;
       this.__strike = Number.parseFloat(strike);
-      this.__cost = Number.parseFloat(price) * Number.parseInt(amount);
       this.__amount = Number.parseInt(amount);
-      this.__average = Number.parseFloat(price);
+      this.__average = Number.parseFloat(price) || Number.parseFloat(average);
     } catch (err) {
       throw Error("internal#NAN");
     }
@@ -26,22 +31,109 @@ export class Trade {
     this.__date = date;
     this.__type = type.toUpperCase();
     this.__owner = owner;
+    this.__sl = undefined;
+    this.__profit = profit;
+    this.__status = status; // > 0 means open
+    this.__cost = cost || this.__average * this.__amount;
+    this.__tp = undefined;
+    this.__trim = "";
+    this.__profitPercent = profitPercent;
   }
 
-  average(price, amount) {
-    this.__cost += price * amount;
-    this.__amount += amount;
-    this.__average = this.round(this.cost / this.amount);
+  get amount() {
+    return this.__amount;
   }
 
-  round(value) {
-    // rounds to the nearest cent
-    value = Math.round(value * 100) / 100;
-    return value;
+  set amount(amount) {
+    this.__amount = amount;
   }
 
-  format(value) {
-    return `${value < 1 ? `0${value}` : `${value}`}`;
+  get average() {
+    return this.__average;
+  }
+
+  set average(price) {
+    this.__average = price;
+  }
+
+  get id() {
+    return this.__id;
+  }
+
+  set id(id) {
+    this.__id = id;
+  }
+
+  get date() {
+    return this.__date;
+  }
+
+  set date(date) {
+    this.__date = date;
+  }
+
+  get type() {
+    return this.__type;
+  }
+
+  set type(type) {
+    this.__type = type;
+  }
+
+  get owner() {
+    return this.__owner;
+  }
+
+  set owner(owner) {
+    this.__owner = owner;
+  }
+
+  get sl() {
+    return this.__sl;
+  }
+
+  set sl(sl) {
+    this.__sl = sl;
+  }
+
+  get profit() {
+    return this.__profit;
+  }
+
+  set profit(profit) {
+    this.__profit = profit;
+  }
+
+  get status() {
+    return this.__status;
+  }
+
+  set status(status) {
+    this.__status = status;
+  }
+
+  get cost() {
+    return this.__cost;
+  }
+
+  set cost(cost) {
+    this.__cost = cost;
+  }
+
+  get tp() {
+    return this.__tp;
+  }
+
+  set tp(tp) {
+    this.__tp = tp;
+  }
+
+  get trim() {
+    return this.__trim;
+  }
+
+  set trim(trim) {
+    this.__trim = trim;
   }
 
   get info() {
@@ -54,8 +146,50 @@ export class Trade {
       amount: this.__amount,
       average: this.format(this.__average),
       owner: this.__owner,
+      sl: this.__sl,
+      profit: this.__profit,
+      status: this.__status,
+      cost: this.__cost,
+      profitPercent: this.__profitPercent,
     };
 
     return data;
+  }
+
+  get profitPercent() {
+    return this.__profitPercent;
+  }
+
+  set profitPercent(percent) {
+    this.__profitPercent = percent;
+  }
+
+  toProfitPercentString() {
+    return `Trim ${this.__trim} TP @ ${this.__tp} for ${this.percentify((this.__tp - this.__average) / this.__average)}`;
+  }
+
+  buy(price, amount) {
+    const newCost = price * amount;
+    this.__cost += newCost;
+    this.__amount += amount;
+    this.__average = round(this.cost / this.amount);
+  }
+
+  toString() {
+    return `${this.__status ? "OPEN" : "CLOSED"} $${this.__ticker} ${this.__date} ${this.__strike}${this.__type} @ ${this.format(this.__average)} ${
+      this.__sl ? `\nStop: ${this.__sl}` : ""
+    }`;
+  }
+
+  takeProfit(price, amount = 1) {
+    this.__profit = round((price - this.__average) * amount);
+    this.__trim = toPercent(amount / this.__amount, 0);
+    this.__amount -= amount;
+    this.__tp = price;
+    this.__profitPercent = toPercent(this.__profit / this.__cost);
+    if (!this.__amount) {
+      this.__status = false; // means closed
+    }
+    this.__cost = this.__cost - amount * this.__average;
   }
 }
---
+++ utils/utils.js
@@ -0,0 +1,14 @@
+export const round = (value) => {
+  // rounds to the nearest cent
+  value = Math.round(value * 100) / 100;
+  return value;
+};
+
+export const format = (value = 0, precision = 2) => {
+  return `${value.toFixed(precision)}`;
+};
+
+export const toPercent = (value, precision = 2) => {
+  // value should be 0.XX where value are the XX
+  return `${format(value * 100, precision)}%`;
+};
