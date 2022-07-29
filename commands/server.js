95182f4a6cff6e69d5566dec9e06af0b205fadda
Victor Tran <victor-likes-coding@tempestas.io>
2022-07-29 18:22:51 -0500

    remove lucyembed import

--- commands/server.js
+++ commands/server.js
@@ -1,17 +1,11 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { LucyEmbed } from "../src/model/Message/LucyEmbed.js";
+import { SlashCommandBuilder } from "discord.js";
 
 export const command = {
-  data: new SlashCommandBuilder()
-    .setName("server")
-    .setDescription(
-      "Provides information about the server, such as members, server level, etc."
-    ),
-  async execute(interaction) {
-    const message = new LucyEmbed({
-      title: "Server Command",
-      description: `Server: ${interaction.guild.name}\nMembers: ${interaction.guild.memberCount}\nMore information coming...`,
-    });
-    return interaction.reply({ embeds: [message.content] });
-  },
+    data: new SlashCommandBuilder().setName("server").setDescription("Provides information about the server, such as members, server level, etc."),
+    async execute(interaction) {
+        await interaction.reply({
+            content: `Server: ${interaction.guild.name}\nMembers: ${interaction.guild.memberCount}\nMore information coming...`,
+            ephemeral: true,
+        });
+    },
 };
