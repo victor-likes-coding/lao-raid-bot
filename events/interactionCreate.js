cb7505906cfa302f743869543fe3ea009d834b22
Victor Tran <victor-likes-coding@tempestas.io>
2022-07-29 18:22:23 -0500

    remove import

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,25 +1,21 @@
-import { ErrorMessage } from "../src/model/Message/ErrorMessage.js";
-
 export const event = {
-  name: "interactionCreate",
-  async execute(interaction) {
-    if (!interaction.isCommand()) return;
+    name: "interactionCreate",
+    async execute(interaction) {
+        if (!interaction.isCommand()) return;
 
-    if (!interaction.client.commands.has(interaction.commandName)) return;
+        if (!interaction.client.commands.has(interaction.commandName)) return;
 
-    try {
-      return interaction.client.commands
-        .get(interaction.commandName)
-        .execute(interaction);
-    } catch (error) {
-      const message = new ErrorMessage({
-        title: "Error",
-        description: "There was an error while executing this command!",
-      });
-      await interaction.reply({
-        embeds: [message.content],
-        ephemeral: true,
-      });
-    }
-  },
+        try {
+            return interaction.client.commands.get(interaction.commandName).execute(interaction);
+        } catch (error) {
+            const message = new ErrorMessage({
+                title: "Error",
+                description: "There was an error while executing this command!",
+            });
+            await interaction.reply({
+                embeds: [message.content],
+                ephemeral: true,
+            });
+        }
+    },
 };
