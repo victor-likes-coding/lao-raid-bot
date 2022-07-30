835e69f7a8cbff78c0fbdfd3bfa459622562d33f
Victor Tran <victor-likes-coding@tempestas.io>
2022-07-30 18:53:40 -0500

    initialize db

--- utils/client.js
+++ utils/client.js
@@ -4,6 +4,19 @@ import { REST } from "@discordjs/rest";
 import { config } from "../config.js";
 import { Raid } from "../src/model/Raid.js";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { config as firebaseConfig } from "../firebase.config.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
 const options = {
     intents: [GatewayIntentBits.Guilds],
 };
