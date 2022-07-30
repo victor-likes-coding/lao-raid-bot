4a3b50e04a6206357d47e9fbf36786f65df5e504
Victor Tran <victor-likes-coding@tempestas.io>
2022-07-30 11:09:16 -0500

    interact with selection

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,21 +1,31 @@
+import { raid } from "../src/model/Raid.js";
+
 export const event = {
     name: "interactionCreate",
     async execute(interaction) {
-        if (!interaction.isCommand()) return;
+        // check if command
+        if (interaction.isCommand()) {
+            if (!interaction.client.commands.has(interaction.commandName)) return;
 
-        if (!interaction.client.commands.has(interaction.commandName)) return;
+            try {
+                return interaction.client.commands.get(interaction.commandName).execute(interaction);
+            } catch (error) {
+                const message = new ErrorMessage({
+                    title: "Error",
+                    description: "There was an error while executing this command!",
+                });
+                await interaction.reply({
+                    embeds: [message.content],
+                    ephemeral: true,
+                });
+            }
+        }
 
-        try {
-            return interaction.client.commands.get(interaction.commandName).execute(interaction);
-        } catch (error) {
-            const message = new ErrorMessage({
-                title: "Error",
-                description: "There was an error while executing this command!",
-            });
-            await interaction.reply({
-                embeds: [message.content],
-                ephemeral: true,
-            });
+        // check if selectmenu interaction
+        if (interaction.isSelectMenu()) {
+            await interaction.deferReply();
+            if (interaction.customId === "raid")
+                await interaction.editReply({ content: `You've chosen: ${raid.menus["raid"][Number.parseInt(interaction.values[0])].label}`, ephemeral: true });
         }
     },
 };
