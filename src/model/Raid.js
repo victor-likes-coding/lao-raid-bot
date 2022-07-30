9fec1dc7ec6c674649f0c071e8bb7c4d91bff595
Victor Tran <victor-likes-coding@tempestas.io>
2022-07-30 11:57:54 -0500

    turn Raid into static class

--- src/model/Raid.js
+++ src/model/Raid.js
@@ -8,15 +8,19 @@ for (let i = 0; i < 24; i++) {
 
 export class Raid {
     // tag should be a discord tag
-    menus = {};
-    selectMenus = {};
+    static menus = {};
+    static selectMenus = {};
+    static __currentUpdater = "";
+    static __updaterId = null;
+    static __raid = "";
+    static __date = "";
+    static __time = "";
 
     constructor(tag) {
         this.host = tag;
-        this.setup();
     }
 
-    generateMenu = (list) => {
+    static generateMenu = (list) => {
         return list.reduce((prev, current, index) => {
             return prev.concat({
                 label: current,
@@ -25,24 +29,40 @@ export class Raid {
         }, []);
     };
 
-    setup = () => {
+    static setup = () => {
         // sets up the menus of the raid object
 
         // we have actual raids menu and a dates menu as well as a time menu
         // raid menu
-        this.menus["raid"] = this.generateMenu(["Argos P1", "Argos P2", "Argos P3", "Valtan NM", "Valtan HM", "Vykas NM", "Vykas HM"]);
-        this.menus["date"] = this.generateMenu(["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]);
-        this.menus["time"] = this.generateMenu(times);
+        this.menus["raid"] = Raid.generateMenu(["Argos P1", "Argos P2", "Argos P3", "Valtan NM", "Valtan HM", "Vykas NM", "Vykas HM"]);
+        this.menus["date"] = Raid.generateMenu(["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"]);
+        this.menus["time"] = Raid.generateMenu(times);
 
         // selectmenus
-        this.selectMenus["raid"] = this.createSelectMenus("raid", "Select Raid");
-        this.selectMenus["date"] = this.createSelectMenus("date", "Select Date");
-        this.selectMenus["time"] = this.createSelectMenus("time", "Select Time");
+        this.selectMenus["raid"] = Raid.createSelectMenus("raid", "Select Raid");
+        this.selectMenus["date"] = Raid.createSelectMenus("date", "Select Date");
+        this.selectMenus["time"] = Raid.createSelectMenus("time", "Select Time");
     };
 
-    createSelectMenus = (type, placeholder) => {
+    static createSelectMenus = (type, placeholder) => {
         return new ActionRowBuilder().addComponents(new SelectMenuBuilder().setCustomId(type).setPlaceholder(placeholder).addOptions(this.menus[type]));
     };
-}
 
-export const raid = new Raid();
+    static getUpdater = () => {
+        return this.__currentUpdater;
+    };
+
+    static addUpdater = (tag) => {
+        this.__currentUpdater = tag;
+        return this;
+    };
+
+    static getUpdaterId = () => {
+        return this.__updaterId;
+    };
+
+    static addUpdaterId = (id) => {
+        this.__updaterId = id;
+        return this;
+    };
+}
