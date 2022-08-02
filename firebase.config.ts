a1c98d653c988bb8c354ec2db6e4fa0b33ef9238
Victor Tran <victor-likes-coding@tempestas.io>
2022-08-02 13:32:23 -0500

    convert to ts

---
+++ firebase.config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyAu_fhm5nGYcqVvc8mYrAW_Iw5NAe7EasI",
+    authDomain: "lao-raid-assistant.firebaseapp.com",
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://DATABASE_NAME.firebaseio.com",
+    projectId: "lao-raid-assistant",
+    storageBucket: "lao-raid-assistant.appspot.com",
+    messagingSenderId: "182798052009",
+    appId: "APP_ID",
+};
