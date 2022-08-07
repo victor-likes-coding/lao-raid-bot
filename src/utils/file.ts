d28852f0af6c3231c698424acc630f6d5bc4c910
Victor Tran <victor-likes-coding@tempestas.io>
2022-08-07 18:29:25 -0500

    rename and add fileExists helper function

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,8 +1,12 @@
 import fs from "fs";
 export const read = (path: string) => {
-    if (fs.existsSync(path)) {
+    if (fileExists(path)) {
         return fs.readdirSync(path).filter((file) => file.endsWith(".ts"));
     }
     console.log(`${path} doesn't exist??`);
     return [];
+};
+
+export const fileExists = (path: string) => {
+    return fs.existsSync(path);
 };
