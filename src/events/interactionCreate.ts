ff6eb3fb7f71655993d261faa1ae0e67fa060285
Victor Tran <victor-likes-coding@tempestas.io>
2022-08-28 12:32:40 -0500

    extract raid id from interaction

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -2,6 +2,13 @@ export const event = {
     name: "interactionCreate",
     async execute(interaction: any) {
         // check if chat inputs
+        if (interaction.isSelectMenu()) {
+            const selectMenuInteraction: SelectMenuInteraction = interaction;
+            if (selectMenuInteraction.customId === "raid") {
+                // we now know which raid id
+                const [id] = selectMenuInteraction.values;
+        }
+
         if (interaction.isChatInputCommand()) {
             if (!interaction.client.commands.has(interaction.commandName)) return;
 
